330f18d096f1ccb079a4d17e1017bb4aeb444cd5
alctclub <35948621+alctclub@users.noreply.github.com>
2018-08-13 09:39:30 +0800

    remove code

--- build-single-image.sh
+++ build-single-image.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
 
-cloudPrefix=ccr.ccs.tencentyun.com
-localPrefix=docker.quncrm.com
-if [ "$buildToCloud" = true ]; then 
-  docker login --username=100001370990 ccr.ccs.tencentyun.com --password=abcd1234
-  prefix=$cloudPrefix
-else
-  # docker login --username=admin docker.quncrm.com --password=abc123_
-  prefix=$localPrefix  
-fi
-
-docker run --rm -i -v $(pwd):/app docker.quncrm.com/base/dotnet:2.0.0-sdk-jessie sh -c "cd /app/ALCT.Wechat.Mini.Program && dotnet build --force && cd /app/ALCT.Wechat.Mini.Program && dotnet publish"
-docker build -t $prefix/alct/alct-wxmp:$releaseVersion ALCT.Wechat.Mini.Program
-docker push $prefix/alct/alct-wxmp:$releaseVersion
